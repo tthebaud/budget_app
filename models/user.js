3908ad4786ef487792ff41172f6f9dbe48898e49
tthebaud <tthebaud@hawk.iit.edu>
2019-03-22 23:41:17 -0500

    Create User model and migration

---
+++ models/user.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = ( sequelize, DataTypes ) => {
+  let User = sequelize.define( 'User', {
+    id: {
+      type: DataTypes.UUIDV4,
+      defaultValue: DataTypes.UUIDV4,
+
+      allowNull: false,
+      primaryKey: true
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: true
+    }
+    firstname: {
+      type: DataTypes.STRING,
+      allowNull: true
+    }
+    lastname: {
+      type: DataTypes.STRING,
+      allowNull: true
+    }
+    password: {
+      type: DataTypes.STRING,
+      allowNull: true
+    }
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    }
+  })
+  return User;
+}
