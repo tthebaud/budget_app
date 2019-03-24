7d4b217445e251502bbd01a6af10174d43f182e0
tthebaud <tthebaud@hawk.iit.edu>
2019-03-23 19:48:25 -0500

    Created migrations for creating Bills and Incomes tables

---
+++ migrations/201903231947-CreateIncomesTable.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  up: ( queryInterface, Sequelize ) => {
+    return queryInterface.createTable( 'Incomes', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      name: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      date: {
+        allowNull: true,
+        type: Sequelize.DATE
+      },
+      amount: {
+        allowNull: false,
+        type: Sequelize.DOUBLE
+      },
+    });
+  },
+  down: ( queryInterface, Sequelize ) => {
+    return queryInterface.dropTable( 'Incomes' );
+  }
+};
